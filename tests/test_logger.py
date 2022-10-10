c7b14bf7d81977369c2bfbd1d2595793fd64f3b1
Amanuel3065 <amanuelzewdu21@gmail.com>
2022-10-10 22:10:17 +0000

    Testing

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -13,5 +13,5 @@ class TestCases(unittest.TestCase):
     def test_logging(self):
         with self.assertLogs() as captured:
             AppLog.get_app_logger(self)
-        self.assertEqual(len(captured.records), 1) 
-        self.assertEqual(captured.records[0].getMessage(), "Hello, World!") 
+        self.assertEqual(len(captured.logger), 1) 
+        self.assertEqual(captured.logger[0].getMessage(), "Hello, World!") 
