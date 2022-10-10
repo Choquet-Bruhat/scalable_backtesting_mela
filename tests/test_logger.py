f4fd2e991afeb22fa37e585eff18ff309026f33c
Amanuel3065 <amanuelzewdu21@gmail.com>
2022-10-10 22:19:57 +0000

    Testing

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -8,10 +8,18 @@ sys.path.append(os.path.abspath(os.path.join('scripts')))
 
 from log_helper import AppLog
 
+mes = "Test me"
 
 class TestCases(unittest.TestCase):
     def test_logging(self):
-        with self.assertLogs() as captured:
-            AppLog.get_app_logger(self)
-        self.assertEqual(len(captured.logger), 1) 
-        self.assertEqual(captured.logger[0].getMessage(), "Hello, World!") 
+        tf = AppLog(mes)
+        self.assertEqual(mes.get_app_logger(), tf.mes.get_app_logger())
+    #     self.assert
+    #     with self.assertLogs() as captured:
+    #         AppLog.get_app_logger(self)
+    #     self.assertEqual(len(captured.logger), 1) 
+    #     self.assertEqual(captured.logger[0].getMessage(), "Hello, World!") 
+
+    # def test_class_creation(self):
+    #     data_preProcessing = dataProcessor(df)
+    #     self.assertEqual(df.info(), data_preProcessing.df.info())
