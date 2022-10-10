6c5460571690b0a873086e75543f8e974b46c913
Amanuel3065 <amanuelzewdu21@gmail.com>
2022-10-10 22:46:09 +0000

    Testing

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,23 +1,28 @@
+# import logging
+# import unittest  
+# from unittest.mock import patch
+
+# import sys,os
+
+# sys.path.insert(0, '../scripts/')
+# sys.path.append(os.path.abspath(os.path.join('scripts')))
+
+# from log_helper import AppLog
+
+
+import sys
+import unittest
 import logging
-import unittest  
-from unittest.mock import patch
 
-import sys,os
+logger = logging.getLogger()
+logger.level = logging.DEBUG
 
-sys.path.insert(0, '../scripts/')
-sys.path.append(os.path.abspath(os.path.join('scripts')))
-
-from log_helper import AppLog
-
-
-
-
-class TestCases(unittest.TestCase):
-    @patch("log_helper.logging.info")
-    def test_logging(self):
-        AppLog.get_app_logger()
-        self.assert_called_once_with("Hello, World!")
-
-   
-
-        
+class TestCase(unittest.TestCase):
+    def testSimpleMsg(self):
+        stream_handler = logging.StreamHandler(sys.stdout)
+        logger.addHandler(stream_handler)
+        try:
+            print("AA")
+            logging.getLogger().info("BB")
+        finally:
+            logger.removeHandler(stream_handler)
