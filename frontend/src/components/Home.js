2e1566f0c3590e07232bab2131f9d91d9db5383b
michaelgetachew-abebe <mikygetyos@gmail.com>
2022-10-13 09:16:35 +0000

    Feat(add): modifications

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const HomePage=()=>{
+    return(
+        <div className = "home">
+            <h1>Welcome to Crypto Trading Engineering</h1>
+        </div>
+    )
+}
