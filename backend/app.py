d6ac0edc588a533dcd5275f66842112c68b8855a
michaelgetachew-abebe <mikygetyos@gmail.com>
2022-10-13 19:32:14 +0000

    Feat(add): added the dummy backtest list endpoint

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 backtest_metrics = {
     1: {"return": 1500, "Number_of_trades": 50, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353},
-    2: {"return": 1500, "Number_of_trades": 50, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353},
+    2: {"return": 1700, "Number_of_trades": 40, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353},
     3: {"return": 1500, "Number_of_trades": 50, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353},
     4: {"return": 1500, "Number_of_trades": 50, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353}
 }
@@ -15,8 +15,12 @@ class backtest(Resource):
     def get(self, backtest_id):
         return backtest_metrics[backtest_id]
 
+class backtests(Resource):
+    def get(self):
+        return backtest_metrics
+        
 api.add_resource(backtest, '/backtest/<int:backtest_id>')
-
+api.add_resource(backtests, '/backtests')
 
 if __name__ == '__main__':
     app.run(debug=True)
