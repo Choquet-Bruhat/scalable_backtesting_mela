83ae33ecc94e796ffb1c8290b986699092c13b72
michaelgetachew-abebe <mikygetyos@gmail.com>
2022-10-13 15:01:48 +0000

    Feat(add): a basic get request handeler

--- backend/app.py
+++ backend/app.py
@@ -4,5 +4,19 @@ from flask_restful import Resource, Api, reqparse, abort
 app = Flask(__name__)
 api = Api(app)
 
+backtest_metrics = {
+    1: {"return": 1500, "Number_of_trades": 50, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353},
+    2: {"return": 1500, "Number_of_trades": 50, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353},
+    3: {"return": 1500, "Number_of_trades": 50, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353},
+    4: {"return": 1500, "Number_of_trades": 50, "Winning_Trades": 100, "Losing_Trades": 21, "Max_drawdown": 16, "Sharpe_Ratio": 11.34353}
+}
+
+class backtest(Resource):
+    def get(self, backtest_id):
+        return backtest_metrics[backtest_id]
+
+api.add_resource(backtest, '/backtest/<int:backtest_id>')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
