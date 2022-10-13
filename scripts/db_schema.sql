e4348d25f7b6dc8bf081d02d07439615cadb3d57
Tegisty Hailay <tigisthay13@gmail.com>
2022-10-13 08:56:58 +0300

    database schema

--- scripts/db_schema.sql
+++ scripts/db_schema.sql
@@ -2,5 +2,14 @@ CREATE TABLE "User_Table" (
   "ID" varchar(PK),
   "Password" varchar,
   "Email" varchar,
-  "Subscription_ID" varchar(FK)
+
+);
+CREATE TABLE "Result_Table" (
+  "ID" varchar(PK),
+  "return" varchar,
+  "Number_of_trades" int,
+  "Winning_trades" varchar,
+  "Losing_trades" varchar,
+  "Max_drawdown" varchar,
+  "Sharpe_ratio" varchar
 );
